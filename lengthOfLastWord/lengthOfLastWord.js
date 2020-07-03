d493b6663c94768def7f119929320c489d6e2ebd
M-A-Justice <m_a_justice@yahoo.com>
2020-07-03 11:22:31 -0700

    complete lengthOfLastWord function

---
+++ lengthOfLastWord/lengthOfLastWord.js
@@ -0,0 +1,25 @@
+/*
+Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word (last word means the last appearing word if we loop from left to right) in the string.
+
+If the last word does not exist, return 0.
+
+Note: A word is defined as a maximal substring consisting of non-space characters only.
+
+Example:
+
+Input: "Hello World"
+Output: 5
+*/
+
+var lengthOfLastWord = function(s) {
+    if (s.length === 0) {
+      return 0;
+    }
+    let arrayOfWords = s.split(' ');
+    for (let z = arrayOfWords.length - 1; z >= 0; z--) {
+      if (arrayOfWords[z].length > 0) {
+        return arrayOfWords[z].length;
+      }
+    }
+    return 0;
+  };
