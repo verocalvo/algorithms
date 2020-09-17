c3f904fcb05ea3879b13d4e713f496669081112f
m_a_justice@yahoo.com <m_a_justice@yahoo.com>
2020-09-17 05:56:35 -0700

    added improved runtime implementation of intersectionOfTwoArrays

--- intersectionOfTwoArrays/intersectionOfTwoArrays.js
+++ intersectionOfTwoArrays/intersectionOfTwoArrays.js
@@ -1,3 +1,5 @@
+// **************************************QUICK AND DIRTY******************************************* //
+
 var intersection = function(nums1, nums2) {
   let result = [];
   for (let i = 0; i < nums1.length; i += 1) {
@@ -6,4 +8,28 @@ var intersection = function(nums1, nums2) {
     }
   }
   return result;
+};
+
+
+// *******************************IMPROVED RUNTIME COMPLEXITY************************************* //
+
+let iterator = (set1, set2) => {
+  let array = [];
+  set1.forEach((num) => {
+    if (set2.has(num)) {
+      array.push(num);
+    }
+  });
+  return array;
+};
+
+var intersection = function(nums1, nums2) {
+  let left = new Set(nums1);
+  let right = new Set(nums2);
+  
+  if (left.length > right.length) {
+    return iterator(right, left);
+  } else {
+    return iterator(left, right);
+  }
 };
